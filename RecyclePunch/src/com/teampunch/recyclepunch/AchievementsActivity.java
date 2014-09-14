57c85f50573861b817e3e9751047d9e0c994cbc7
Patrick Owen <patowen95@gmail.com>
2014-09-14 03:17:30 -0400

    Remove unused import in AchievementsActivity.java

--- RecyclePunch/src/com/teampunch/recyclepunch/AchievementsActivity.java
+++ RecyclePunch/src/com/teampunch/recyclepunch/AchievementsActivity.java
@@ -1,11 +1,9 @@
 package com.teampunch.recyclepunch;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 public class AchievementsActivity extends Activity {
 
